f256b9e388ef16281ec278eb7e09a2dcd27c80a8
taiyme <53635909+taiyme@users.noreply.github.com>
2025-08-07 13:07:36 +0900

    a

---
+++ eslint.config.ts
@@ -0,0 +1,29 @@
+import type { Linter } from 'eslint';
+import taiymeConfig from '@taiyme/eslint-config';
+import tsEslintParser from '@typescript-eslint/parser';
+import gitignore from 'eslint-config-flat-gitignore';
+import globals from 'globals';
+
+const files = ['**/*.{js,ts}'];
+
+export default [
+  gitignore(),
+  {
+    name: 'sandbox/typescript',
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+      parser: tsEslintParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    files,
+  },
+  ...taiymeConfig.configs.typescript.map((config) => ({
+    ...config,
+    files,
+  })),
+] as const satisfies Linter.Config[];
